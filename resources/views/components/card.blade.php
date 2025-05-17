367d7e3a2da9decee1c68d771366c67edd929dc1
Marin Milotić <mGitM101@gmail.com>
2025-05-17 19:09:27 +0200

    Card component created

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+<div class="rounded-lg shadow-md bg-red-100 p-10 m-2">
+    <h2 class="text-xl font-semibold">Title goes here</h2>
+    <p class="text-sm text-gray-500">Description goes here</p>
+    <p class="text-md text-gray-700 mt-4">website</p>
+    </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,16 @@
 <x-layout>
     <x-header-banner />
-    <p>This shit's workin???</p>
+    <div class="grid grid-cols-1 md:grid-cols-6 gap-4 mb-6 mt-6">
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+        <x-card />
+    </div>
 </x-layout>
