1adcfff6fe74b1211065de2f8578b4b95da75428
Marin Milotić <mGitM101@gmail.com>
2025-06-05 16:38:42 +0200

    Edited the alert messages

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -5,4 +5,7 @@
         id="{{$id}}" type="{{$type}}" name="{{$name}}" placeholder="{{$placeholder}}" value="{{old($name, $value)}}" {{$required ? 'required' : ''}}
          class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
     />
+    @error($name)
+            <p class="text-red-500 text-sm mt-1">{{$message}}</p>
+    @enderror
 </div>
